bca84923bfa578c48a267532f032edf495888b29
dheerajgundeti <gundetidheeraj@gmail.com>
2017-01-22 16:51:20 +0530

    MAXIMIZING XOR

---
+++ max_xor.c
@@ -0,0 +1,30 @@
+//Given two integers,l and r, find the maximal value of a xor b,where l<=a<=b<=r 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r) {
+        int res=0;
+        for(int i=l;i<=r;i++)
+            for(int j=i+1;j<=r;j++)
+                if((i^j)>=res)
+                     res=i^j;
+        return (res);
+}
+int main() {
+    int res;
+    int l;
+    scanf("%d", &l);
+    
+    int r;
+    scanf("%d", &r);
+    
+    res = maxXor(l,r);
+    printf("%d", res);
+    
+    return 0;
+}
