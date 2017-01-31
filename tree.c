d13dd1b639c7c23f5c8d63fac7886c00dcd5d549
Gundeti Dheeraj <gundetidheeraj@gmail.com>
2017-01-31 22:39:48 +0530

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,3 +1,4 @@
+//problem from hackerrank/algorithms/implementation
 /*Tree goes through 2 cycles of growth every year. Each spring, it doubles in height.
  Each summer, its height increases by 1 meter.
  person plants a Utopian Tree sapling with a height of 1 meter at the onset of spring.
